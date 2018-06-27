fabf89861a2f3c8e019b1e400fdfac1e71a6698a
JDJR\baihe3 <baihe3@jd.com>
2018-06-27 15:04:07 +0800

    add less

--- Sass&Less/devProject/gulpfile.js
+++ Sass&Less/devProject/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var fs = require('fs');
 var sass = require('gulp-ruby-sass');
+var less = require('gulp-less');
 var browserSync = require('browser-sync');
 var through2 = require('through2');
 
@@ -14,12 +15,18 @@ function delBlankLines() {
 };
 
 gulp.task('sass:compile', function() {
-	return sass('./style.scss', {style: 'compact'})
+	return sass('./style_s.scss', {style: 'compact'})
 			.pipe(delBlankLines())
 			.pipe(gulp.dest('./'))
 });
 
-gulp.task('server', ['sass:compile'], function() {
+gulp.task('less:compile', function() {
+	return gulp.src('./style_l.less')
+			.pipe(less())
+			.pipe(gulp.dest('./'))
+});
+
+gulp.task('server', ['sass:compile','less:compile'], function() {
 	browserSync({
 		open: false,
 		server: {
@@ -27,7 +34,8 @@ gulp.task('server', ['sass:compile'], function() {
 		}
 	});
 
-	var watcher = gulp.watch('./*.scss', ['sass:compile']);
+	gulp.watch('./*.scss', ['sass:compile']);
+	gulp.watch('./*.less', ['less:compile']);
 
 	gulp.watch(['./*.html', './*.css', './*.js'], browserSync.reload);
 });
